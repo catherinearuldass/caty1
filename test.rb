7a77785cdc93d03ef1e3168e846c0fabf32729f3
catherine <catherine.aruldass@gmail.com>
2022-12-05 17:56:59 +0530

    made a change

---
+++ test.rb
@@ -0,0 +1,3 @@
+This is interesting, because now your testing branch has moved forward, but your master branch
+still points to the commit you were on when you ran git checkout to switch branches. Let’s switch
+back to the master branch:
