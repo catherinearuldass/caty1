5ff62b9339d100213bf6deb1817d52f396f7c98b
catherine <catherine.aruldass@gmail.com>
2022-12-05 17:59:39 +0530

    made a change

---
+++ test1.rb
@@ -0,0 +1,4 @@
+It’s important to note that when you switch branches in Git, files in your working
+directory will change. If you switch to an older branch, your working directory
+will be reverted to look like it did the last time you committed on that branch. If Git
+cannot do it cleanly, it will not let you switch at all
